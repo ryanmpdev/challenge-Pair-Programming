20c6bd1d999826dab3b06bd53d72767f8d2168b7
ryanmpdev <ryanmaciel697@gmail.com>
2022-09-28 16:38:08 -0300

    update footer

--- desafio-dupla/src/components/Footer/index.js
+++ desafio-dupla/src/components/Footer/index.js
@@ -5,12 +5,26 @@ import { Instagram, Pinterest, Facebook, Twitter } from '../../images'
 function Footer() {
   return (
     <S.Container>
+      <S.Section>
     <S.Redes>
       <img src={Instagram} alt="instagram" />
       <img src={Twitter} alt="Twiter" />
       <img src={Facebook} alt="Facebook" />
       <img src={Pinterest} alt="Pinterest" />
     </S.Redes>
+    <S.Infos>
+      <nav>    
+      <ul>
+        <li>ABOUT</li>
+        <li>RECIPES</li>
+        <li>SUBSCRIBE</li>
+      </ul>
+      </nav>
+    </S.Infos>
+      </S.Section>
+    <S.End>
+      <p>Layout produzido por Vai na Web para fins exclusivamente educacionais. Referência: https://br.pinterest.com/pin/AVuDlMAl4GsQiM6nijH9YbG9bsNKpompSEOEHzig6GJ58AnUtMkSy7k/</p>
+    </S.End>
    </S.Container>
   )
 }
